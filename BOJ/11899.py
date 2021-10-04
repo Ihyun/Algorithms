f89fe6c1de57a6350c90aee3326e03f225c5fefb
Ihyun <ju9699s@gmail.com>
2021-10-04 16:48:17 +0900

    BOJ 11899, second solution

--- BOJ/11899.py
+++ BOJ/11899.py
@@ -1,17 +1,15 @@
-# Q: https://www.acmicpc.net/problem/11899
+# https://www.acmicpc.net/problem/11899
 
 s = input()
 val = 0
-vals = [0]
+minval = 0
 
 for c in s:
     if c == '(':
         val += 1
-        vals.append(val)
     elif c == ')':
         val -= 1
-        vals.append(val)
+        if minval > val:
+            minval = val
 
-ans = min(vals) * -1
-ans += vals[-1] + ans
-print(ans)
+print(minval * -2 + val)
