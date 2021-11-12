c7b11758731af8abe0de01e73362f240d082f76c
Ihyun <ju9699s@gmail.com>
2021-11-13 02:15:30 +0900

    BOJ 1535

---
+++ BOJ/1535.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/1535
+
+global N
+global hps
+global dls
+global sol
+
+N = int(input())
+hps = list(map(int, input().split()))
+dls = list(map(int, input().split()))
+sol = 0
+
+
+def chk(i, hp, dl):
+    global sol
+
+    if hp <= 0:
+        return
+
+    if i == N:
+        if sol < dl:
+            sol = dl
+        return
+
+    chk(i+1, hp-hps[i], dl+dls[i])
+    chk(i+1, hp, dl)
+    return
+
+
+chk(0, 100, 0)
+print(sol)
